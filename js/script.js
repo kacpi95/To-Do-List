605e48e9d07bf689c5e6974902c3c697ef793d8a
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-03-10 11:44:15 +0100

    Add new variables in JavaScript files

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const todoInput = document.querySelector(".todo-input");
+const errorInfo = document.querySelector(".error-info");
+const todoUlList = document.querySelector(".todolist ul");
+const newTodoLiList = ''
+const btnAdd = document.querySelector(".btn-add");
+const btnDone = document.querySelector(".complete");
+const btnEdit = document.querySelector(".edit");
+const btnDelete = document.querySelector(".delete");
+const popup = document.querySelector(".popup");
+const popupInput = document.querySelector(".popup-input");
+const popupInfo = document.querySelector('.popup-info')
+const popupBtnAdd = document.querySelector(".accept");
+const popupBtnCancel = document.querySelector(".cancel");
+
+
