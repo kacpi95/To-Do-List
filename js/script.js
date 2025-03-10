9dbcf6d924837869ba93521ebde59f5ac46fff61
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-03-10 12:27:00 +0100

    Add function clear input after adding a new task

--- js/script.js
+++ js/script.js
@@ -3,15 +3,26 @@
 const todoInput = document.querySelector(".todo-input");
 const errorInfo = document.querySelector(".error-info");
 const todoUlList = document.querySelector(".todolist ul");
-const newTodoLiList = ''
 const btnAdd = document.querySelector(".btn-add");
 const btnDone = document.querySelector(".complete");
 const btnEdit = document.querySelector(".edit");
 const btnDelete = document.querySelector(".delete");
 const popup = document.querySelector(".popup");
 const popupInput = document.querySelector(".popup-input");
-const popupInfo = document.querySelector('.popup-info')
+const popupInfo = document.querySelector(".popup-info");
 const popupBtnAdd = document.querySelector(".accept");
 const popupBtnCancel = document.querySelector(".cancel");
 
+const addNewTodo = () => {
+	if (todoInput.value !== "") {
+		const newTodoLiList = document.createElement("li");
+		newTodoLiList.textContent = todoInput.value;
 
+		todoUlList.append(newTodoLiList);
+		todoInput.value = "";
+	} else {
+		errorInfo.textContent = `Wpisz treść zadania`;
+	}
+};
+
+btnAdd.addEventListener("click", addNewTodo);
